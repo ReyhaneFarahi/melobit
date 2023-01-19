d7d87fa5d487788857785f16cff06264d0b89378
ReyhaneFarahi <reyhaneh77337@gmail.com>
2023-01-19 17:36:00 +0330

    search done

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,24 +58,25 @@ const Search=()=>{
   <path d="M11 6.64a1 1 0 0 0-1.243-.97l-1 .25A1 1 0 0 0 8 6.89v4.306A2.572 2.572 0 0 0 7 11c-.5 0-.974.134-1.338.377-.36.24-.662.628-.662 1.123s.301.883.662 1.123c.364.243.839.377 1.338.377.5 0 .974-.134 1.338-.377.36-.24.662-.628.662-1.123V8.89l2-.5V6.64z"/>
   <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
 </svg>
+<div></div>
 
       {
        searchResult.map((s)=>(
           
             s.type==="song" && 
-            <div className='col-md-3 col-sm-11 col-11 text-center mt-4' key={s.position}>
+            <div className='col-md-3 col-sm-11 col-11 text-center mt-4 card-style-search' key={s.position}>
                    <Link to={`details/${s.song.id}`} aria-label="Close" className='text-decoration-none text-dark'>
 
-            <div className="card w-100">
-  <img className='card-img-top' src={s.song.album.image.cover.url} alt={s.song.album.name.replace('Single' , '')}/>
+            <div className="card w-20 card-img-top card-img-top-search">
+  <img className='card-img-top card-img-top-search' src={s.song.album.image.cover.url} alt={s.song.album.name.replace('Single' , '')}/>
 
   <div className="card-body">
     <p className="card-text">
-    <h6 className=' text-dark'>
+    <h6 className=' text-light'>
                       {s.song.album.name.replace('Single' , '')}
                      
                      </h6>
-                     <h6 className=' text-dark'>
+                     <h6 className=' text-light'>
                      {s.song.album.artists[0].fullName}
                      </h6>
                              </p>
